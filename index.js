d294d2ddf2ab9d864407d27856d525789f92c5d6
Sharmaine Perea <sharmaine.perea23@gmail.com>
2025-05-16 17:50:35 -0600

    added basketball stats

--- index.js
+++ index.js
@@ -114,3 +114,172 @@ function gameObject() {
         },
     };
 }
+
+//1. Player Information: 
+
+//Take Players Name and Return Points
+function numPointsScored(playerName) {
+    const game = gameObject();
+    let homePlayers = game.home.players;
+    let awayPlayers = game.away.players;
+    let points;
+
+    Object.keys(homePlayers).forEach((key) => {
+        if(playerName === key) {
+            points = homePlayers[key].points;
+        }
+    }) 
+
+     Object.keys(awayPlayers).forEach((key) => {
+        if(playerName === key) {
+            points = awayPlayers[key].points;
+        }
+    }) 
+
+    return points;
+}
+
+const playerPoints = numPointsScored('Alan Anderson');
+console.log(playerPoints);
+
+//Take Players Name and Return Shoe Size
+function shoeSize(playerName) {
+    const game = gameObject();
+    let homePlayers = game.home.players;
+    let awayPlayers = game.away.players;
+    let shoe;
+
+    Object.keys(homePlayers).forEach((key) => {
+        if(playerName === key) {
+            shoe = homePlayers[key].shoe;
+        }
+    }) 
+
+     Object.keys(awayPlayers).forEach((key) => {
+        if(playerName === key) {
+            shoe = awayPlayers[key].shoe;
+        }
+    }) 
+
+    return shoe;
+}
+
+const playerShoe = shoeSize('Jeff Adrien');
+console.log(playerShoe);
+
+//2. Team Information:
+
+//Take Team Name and Returns ARRAY of Team Colors
+function teamColors(teamName) {
+    const game = gameObject();
+    if(game.home.teamName === teamName) {
+        return game.home.colors;
+    }
+    else{
+        return game.away.colors;
+    }
+}
+
+const colors = teamColors('Brooklyn Nets');
+console.log(colors);
+
+//Returns an ARRAY of Both Team Names
+function teamNames() {
+    const teamNamesArray = []
+    let game = gameObject()
+    teamNamesArray.push(game.home.teamName)
+    teamNamesArray.push(game.away.teamName)
+
+    return teamNamesArray
+}
+
+console.log(teamNames());
+
+//3. Retrieve Players Numbers and Stats
+
+//Takes a Team Name and Returns an ARRAY of All Players Jersey Numbers on that team
+function playerNumbers(teamName) {
+    const game = gameObject();
+    let homePlayers = game.home.players
+    let awayPlayers = game.away.players
+    const jerseyNumbers = []
+
+    if(game.home.teamName === teamName) {
+        for (const key in homePlayers){
+            jerseyNumbers.push(homePlayers[key]['number'])
+        }
+    } 
+    else {
+        for (const key in awayPlayers)
+            jerseyNumbers.push(awayPlayers[key]['number'])
+        }
+
+      return jerseyNumbers
+    }
+    
+const playerNum = playerNumbers('Charlotte Hornets');
+console.log(playerNum);
+
+// Takes a Players Name and Returns an OBJECT with All Stats for That Player
+function playerStats(playerName) {
+    let game = gameObject()
+    let homePlayers = game.home.players
+    let awayPlayers = game.away.players
+
+    if(Object.keys(homePlayers).includes(playerName)) {
+        return homePlayers[playerName]
+    } else {
+        return awayPlayers[playerName]
+    }
+}
+const stats =playerStats("Brook Lopez");
+console.log(stats);
+
+//4. Advanced Challenge:
+
+//Returns the number of rebounds for the player with the largest shoe size
+//Identify the player with the largest shoe size
+function bigShoeRebounds() {
+    const game = gameObject();
+    let homePlayers = game.home.players;
+    let awayPlayers = game.away.players;
+    let shoeSizes = [];
+   
+    
+
+    for(const key in homePlayers){
+        shoeSizes.push(homePlayers[key]['shoe']);
+    }
+
+    for(const key in awayPlayers){
+            shoeSizes.push(awayPlayers[key]['shoe']);
+        }
+
+    //Find Max Shoe Size
+     let maxShoe = shoeSizes[0];
+
+    for(let i =1; i < shoeSizes.length; i++) {
+        if (shoeSizes[i] > maxShoe){
+            maxShoe = shoeSizes[i];
+        }
+    }
+    
+    //Max shoe size display number of rebounds
+    let numRebounds;
+      for (const key in homePlayers) {
+        if(homePlayers[key].shoe === maxShoe){
+            return homePlayers[key].rebounds
+        }
+    }
+
+    for (const key in homePlayers) {
+        if(awayPlayers[key].shoe === maxShoe){
+            return awayPlayers[key].rebounds
+        } 
+    }
+
+    return numRebounds;
+}
+console.log(bigShoeRebounds());
+
+
